add46663390fb7e2c009f43dff00ef636ef88635
Djiouwairiatou Ba <d.b43@zig.univ.sn>
2025-12-29 02:33:57 +0000

    correction front

--- frontend-daos/src/components/layout/Sidebar.jsx
+++ frontend-daos/src/components/layout/Sidebar.jsx
@@ -172,6 +172,7 @@ const Sidebar = () => {
                     <Link to="/seances/search" className={`nav-link ${isActive('/seances/search')}`}>
                         <i className="bi bi-search me-2"></i> Rechercher Séances
                     </Link>
+                </li>
                 {/* --- DÉROULEMENT ENSEIGNEMENT (Dropdown) --- */}
                 <li className="nav-item mb-1">
                     <a className={`nav-link d-flex justify-content-between align-items-center ${isParentActive(['/lst-seances', '/progression', '/statistiques'])}`}
